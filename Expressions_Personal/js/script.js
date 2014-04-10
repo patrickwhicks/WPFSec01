a8d90cb0793cca32070868b8dc7c6890b86a0d09
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-10 16:18:26 -0400

    Defining Variables
    I am defining the variable ‘income’

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,3 +5,9 @@
 var name = prompt("Hello, what is your name?");
 
 console.log(name);
+
+//Prompt user for their annual income
+
+var income = prompt(name + ", how much do you make a year?");
+
+console.log(income);
