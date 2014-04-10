af36a1f9bcfa139fd718f2302f2d793b37156b88
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-10 15:23:36 -0400

    Writing js code for industry expression
    declaring variables for the expression assignment

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,8 +1,15 @@
+//Patrick W. Hicks  4/10/14 Expressions Assignment
 
-//Expressions - Industry  This is an expression to determine the employment status at my job based upon profitability
-var numTours = 50; //This represents the amount of tours a sales representative takes during a 60 day work period
-var volWritten = 50000; //This is the amount of volume that the representative wrote during that same 60 day period
-var volCancelled = 25000; //This is the amount of sales that were cancelled within that 60 day period
-var goodBusiness = volWritten - volCancelled; //This is the amount of business the company could process
-var apg = goodBusiness/numTours; //This number (Average Per Guest) represents how profitable the representative is for the company
-console.log(apg);
+
+//Prompt user for name
+
+var name = prompt("Hello, what is your name?");
+
+console.log(name);
+
+//Promt user for number of clients seen in the last 60 days
+
+var numTours = prompt("How many clients did you meet in the last 60 days, " + name +"?");
+
+console.log(numTours);
+
--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,3 @@
+//Patrick W. Hicks  4/10/14 Expressions Assignment
+
 alert("This is in the js file");
