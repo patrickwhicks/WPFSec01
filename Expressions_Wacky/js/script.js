c14da8387aa025e6d48b6dc98d5e28a09979d8b8
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-10 18:48:07 -0400

    Wacky Expression
    I have outlined the beginning of the first wacky expression

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,8 +1,28 @@
-//Expressions - Wacky
+//Patrick W. Hicks  4/10/14 Expressions Assignment
 
-var distance = 100; //This is the amount of gas that is in the gas tank
-var mileage = 20; //This is how many miles I will get to the gallon
-var price = 3.23; //This is the amount of one gallon of gas
-var cost = (distance/mileage)*price;  //This is how much that road trip with cost
 
-console.log(cost);
+//Prompt user for name
+
+var name = prompt("Hello fellow survivor, what is your name?");
+
+console.log(name);
+
+var first = prompt("Great, " + name + "!  \n Now you need to enter your cohorts names, too! \n What is the name of your first friend?");
+
+console.log(first);
+
+var second = prompt("Doing great!  \n Who else will be taking on the zombies with you?");
+
+console.log(second);
+
+var third = prompt("I hope you have more friends than that that have survived! \n Anyone else, " + name + "?");
+
+console.log(third);
+
+var fourth = prompt("And who, " + name + " is the last player in this game of life?");
+
+console.log(fourth);
+
+alert("Unfortunately, " + name + ", " + fourth + " has been bitten whilest cowering in a dark, damp corner.  You never needed " + fourth + " anyway.");
+
+var walkingDead = [ "first", "second", "third", "fourth"];
