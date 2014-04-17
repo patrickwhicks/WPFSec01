309ee0e0279fc4767574cfe0a555647a1aad0e67
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-17 10:26:19 -0400

    Assigning var gH
    Im prompting the user to assign the gH variable

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,8 +7,14 @@ var name = prompt("Hello future miner!  What is your name?");
 
 console.log(name);
 
-//Prompt use for the bitcoin mining current difficulty rating
+//Prompt user for the bitcoin mining current difficulty rating
 
 var difficulty = Number(prompt(name + ", what is the current bitcoing mining difficulty level?"));
 
 console.log(difficulty);
+
+//Prompt user for the GH/s level of their hardware
+
+var gH = Number(prompt("What is your current hardware's GH/s level, " + name + "?"));
+
+console.log(gH);
