65c4a87f98359f08a3a0b729887acd175a4a3015
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-17 11:05:56 -0400

    Assigning var and changing var hashRate
    I had to bring my var hashRate to a rational number and added the var gH

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -15,6 +15,18 @@ console.log(difficulty);
 
 //Prompt user for the hash rate of their hardware
 
-var hashRate = Number(prompt("What is your current hardware's hash rate, " + name + "?"));
+var gH = Number(prompt("What is your current hardware's hash rate, " + name + "?"));
+
+console.log(gH);
+
+//Translating gH to rational number
+
+var hashRate = Number((gH * 1000000000));
 
 console.log(hashRate);
+
+//Calculating the amount of time to find one share
+
+var shareTime = ((difficulty * 4294967296) / hashRate);
+
+console.log(shareTime);
