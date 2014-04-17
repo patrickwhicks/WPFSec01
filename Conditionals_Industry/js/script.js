d830098fe7e5f364dc3a4d3c658fcc07b9d1f168
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-17 12:53:36 -0400

    Defining the if statements
    I am defining the if statement variables and proceeding to
    maritalStatus var

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -11,3 +11,14 @@ console.log(name);
 
 var age = Number(prompt("Thanks, " + name + "!  How old are you?"));
 
+console.log(age);
+
+var yearsLeft = 23 - age;
+
+console.log(yearsLeft);
+
+if (age < 23) alert("I\'m sorry, " + name + ".  You must be at least 23 years old to tour with Vacation Villiage Resorts. Please return in " + yearsLeft + " years!");
+
+if (age >= 23 ) var maritalSatus = Number(prompt("Great!  Are you married, " + name + "? \n 1. Yes \n 2. No"));
+
+console.log(maritalSatus);
