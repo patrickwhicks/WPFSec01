17155a0f53a0f7df72f0723484565f6efd7f663e
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-17 10:21:57 -0400

    Assigning Difficulty var
    I’m prompting the user to assign the variable for difficulty

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,3 +6,9 @@
 var name = prompt("Hello future miner!  What is your name?");
 
 console.log(name);
+
+//Prompt use for the bitcoin mining current difficulty rating
+
+var difficulty = Number(prompt(name + ", what is the current bitcoing mining difficulty level?"));
+
+console.log(difficulty);
