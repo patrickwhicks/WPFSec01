4abc38bae39540edaf94e1e199300f31abc8e5d0
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-04-17 21:54:21 -0400

    Tried to validate the inputs
    I tried to validate the input fields based upon a “” inputed for the
    prompt, but i couldn’t figure out how to link back to previous code

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,6 +7,10 @@ var name = prompt("Hello fellow prankster, what is your name?");
 
 console.log(name);
 
+//Validating users input
+
+if (name === "") {alert("You need to enter the information please!"); var name = prompt("Hello fellow prankster, what is your name?"); }
+
 //Prompting the user to see how much money he wants to spend
 
 var money = Number(prompt("How many dollars are you willing to spend on this prank, " + name + "?"));
@@ -43,6 +47,8 @@ console.log(peanuts);
 //defining cost of prank
 
 var cost = (officeArea * peanuts);
+
+//Using ternary coding to test the outcome
 
 (cost > money) ? console.log("You may want to just egg his house, " + name + "!  Filling his office with packaging peanuts will cost you $" + cost + ".") : console.log("Go for it, " + name + "! You can always blame it on that guy from accounting!");
 
