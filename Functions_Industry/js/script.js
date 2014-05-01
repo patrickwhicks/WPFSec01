d3de0357ce9d7a36b361a5c6418585e0384ec72c
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-05-01 16:16:05 -0400

    Defining Variables
    I am prompting the user to define the variables needed for the function

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,15 @@
+//Patrick W. Hicks 5/01/14 Function Assignment
+
+//Prompt User for name
+
+var name = prompt("Hello fellow caffine enthusiast!  What is your name?");
+
+
+//Prompt user for the amount of coffee left
+
+var coffeeLeft = Number(prompt("How many bags of coffee do you have left, " + name + "."));
+
+var numberEmployees = Number(prompt("How many employees will be drinking coffee today?"));
+
+var numberCups = Number(prompt("How many cups of coffee can your tired employees drink each?"));
+
