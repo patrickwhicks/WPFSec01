c0d1c70cd1bf5791b094032fa000306bc8f79633
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-05-01 18:35:38 -0400

    Function
    I wrote my function and I am checking my code

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -13,4 +13,8 @@ var vehicle = Number(prompt("What type of vehicle are you traveling in, " + name
 if (vehicle === 1) { var speed = 60; } else if (vehicle === 2) {var speed = 600; } else { var speed = 670616629; }
     
     function calcTravel(){
+        var distance = speed *travelTime; 
+        return distance;
+        console.log("You have traveled exactly " + distance + " miles since you began traveling, " + name + "!");
 }
+console.log(speed);
