2adb93896bcd50a78760b4f7e35359887773f4fe
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-05-01 17:34:38 -0400

    Printing to screen
    I am writing the console.log for the function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,7 +4,7 @@
 
 var name = prompt("Hello fellow caffine enthusiast!  What is your name?");
 
-
+var coffee = Number(prompt("How many bags of coffee do you currently have?"));
 
 //Staring the Anonymous Function
 
@@ -17,5 +17,7 @@ var calcCoffee = function(numberEmployees, numberCups, coffeeUsed) {
 
 var a = calcCoffee(Number(prompt("How many employees will be drinking coffee today?")), Number(prompt("How many cups of coffee can your tired employees drink each?")), Number(prompt("How many bags of coffee does it take per pot to wake your men up, " + name + "?")));
 
+var goBuy = a - coffee; 
 
-console.log(a);
+console.log("You currently have " + coffee + " bags of coffee, " + name + ".  You will need " + a + " bags of coffee to wake your men up!");
+
