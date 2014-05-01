fa4df91991bdc4eda29da22987d951e216d36fd5
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-05-01 14:45:50 -0400

    Defining Variables
    I am defining the name and poolShape variables

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,10 @@
+//Patrick W. Hicks 5/01/14 Personal Functions
+
+//Prompting the user for their name
+
+var name = prompt("Hello! What is your name?");
+
+//Prompting the user for the shape of the pool
+
+var poolShape = Number(prompt(name + ", pick the shape of your pool. \n 1. Rectangle \n 2. Circular"));
+
