cd38cca154cb54bb12d52db9567d1536b7406636
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-05-01 18:20:07 -0400

    Defining Variables
    I am defining variables

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,2 +1,7 @@
 //Patrick W. Hicks  5/01/14 Functions Assignment
 
+//Prompting user for name
+
+var name = prompt("Hello fellow travelor, what is your name?");
+
+var travelTime = prompt(name + ", how many hours have you been traveling for?");
