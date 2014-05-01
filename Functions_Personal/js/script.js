a0e52e057e7fb233e8bc586f63337ac96e9a87bf
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-05-01 15:09:01 -0400

    Defining the conditional
    I am defining the else if conditional for my calculator

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -18,4 +18,12 @@ if (poolShape === 1) { var width = Number(prompt("What is the width of your pool
                     //Printing the result to the console
                     console.log(name + ", you will need " + gallons + " gallons of water to fill your new pool!");
     
-                     }
+                     } //Defining the else if portion of the conditional
+else if (poolShape === 2) { var radius = Number(prompt("What is the radius of your pool?"));
+                            var depth = Number(prompt("What is the average depth of your pool?"));
+                            var gallons = (3.14 * (radius * radius) * depth) * 7.48; 
+                            var gallons = gallons.toFixed(0);
+                           //Printing the result to the console
+                           console.log(name + ", you will need " + gallons + " gallons of water to fill your new pool!");
+                          }
+                                                  
