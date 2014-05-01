30ba6e105642248844ab1da478648b66b2b997b2
Patrick W Hicks <patrickwhicks@fullsail.edu>
2014-05-01 15:00:49 -0400

    Defining Else
    I am defining the else part of my conditional

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -8,3 +8,14 @@ var name = prompt("Hello! What is your name?");
 
 var poolShape = Number(prompt(name + ", pick the shape of your pool. \n 1. Rectangle \n 2. Circular"));
 
+//Starting the else if conditional
+
+if (poolShape === 1) { var width = Number(prompt("What is the width of your pool?")); 
+                    var length = Number(prompt("What is the length of your pool?"));
+                    var depth = Number(prompt("What is the average depth of your pool?"));
+                    var gallons = ((length * width * depth) * 7.48);
+                    
+                    //Printing the result to the console
+                    console.log(name + ", you will need " + gallons + " gallons of water to fill your new pool!");
+    
+                     }
